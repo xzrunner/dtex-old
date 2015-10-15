af0a52b0f01b27e0c044de7b994819f97f764eee
zz <zhuguang@ejoy.com>
2015-10-15 19:06:17 +0800

    [ADDED] c2_strategy

---
+++ dtex_c2_strategy.c
@@ -0,0 +1,8 @@
+//#include "dtex_c2_strategy.h"
+//
+//struct 
+//
+//void 
+//dtex_c2_on_draw_sprite(struct ej_sprite* spr) {
+//	
+//}
---
+++ dtex_c2_strategy.h
@@ -0,0 +1,17 @@
+//#ifdef __cplusplus
+//extern "C"
+//{
+//#endif
+//
+//#ifndef dynamic_texture_c2_strategy_h
+//#define dynamic_texture_c2_strategy_h
+//
+//#include "ejoy2d.h"
+//
+//void dtex_c2_on_draw_sprite(struct ej_sprite* spr);
+//
+//#endif // dynamic_texture_c2_strategy_h
+//
+//#ifdef __cplusplus
+//}
+//#endif
