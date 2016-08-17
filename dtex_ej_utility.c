ca1821b8a287136fb299fde51afa9ae66456c4e4
zz <zhuguang@ejoy.com>
2016-08-17 15:15:03 +0800

    rename

--- dtex_ej_utility.c
+++ dtex_ej_utility.c
@@ -32,14 +32,14 @@ dtex_ej_spr_traverse(struct ej_sprite_pack* ej_pkg, int spr_id, void (*pic_func)
 		}
 	} else if (type == TYPE_PARTICLE3D) {
 		struct p3d_emitter_cfg* p3d_cfg = (struct p3d_emitter_cfg*)ej_pkg->data[spr_id];
-		for (int i = 0; i < p3d_cfg->symbol_count;  ++i) {
-			uint32_t cid = (uint32_t)p3d_cfg->symbols[i].ud;
+		for (int i = 0; i < p3d_cfg->sym_count;  ++i) {
+			uint32_t cid = (uint32_t)p3d_cfg->syms[i].ud;
 			dtex_ej_spr_traverse(ej_pkg, cid, pic_func, ud);
 		}
 	} else if (type == TYPE_PARTICLE2D) {
 		struct p2d_emitter_cfg* p2d_cfg = (struct p2d_emitter_cfg*)ej_pkg->data[spr_id];
-		for (int i = 0; i < p2d_cfg->symbol_count; ++i) {
-			uint32_t cid = (uint32_t)p2d_cfg->symbols[i].ud;
+		for (int i = 0; i < p2d_cfg->sym_count; ++i) {
+			uint32_t cid = (uint32_t)p2d_cfg->syms[i].ud;
 			dtex_ej_spr_traverse(ej_pkg, cid, pic_func, ud);
 		}
 	}
