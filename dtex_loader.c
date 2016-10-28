be0b71f037a8ba9136526427910eea2de3758f51
zz <zhuguang@ejoy.com>
2016-10-28 19:41:17 +0800

    [ADDED] pre mul alpha

--- dtex_loader.c
+++ dtex_loader.c
@@ -446,26 +446,29 @@ dtex_load_file(const char* filepath, void (*unpack_func)(struct dtex_import_stre
 	fs_close(file);
 }
 
+static void 
+_pre_muilti_alpha(uint8_t* pixels, int width, int height) {
+	int pos = 0;
+	for (int y = 0; y < height; ++y) {
+		for (int x = 0; x < width; ++x) {
+			float alpha = pixels[pos + 3] / 255.0f;
+			for (int i = 0; i < 3; ++i) {
+				pixels[pos + i] = (uint8_t)(pixels[pos + i] * alpha);
+			}
+			pos += 4;
+		}
+	}
+}
+
 struct dtex_texture* 
 dtex_load_image(const char* filepath) {
-	printf("dtex_load_image 0\n");
-
 	struct dtex_texture* tex = NULL;
 	if (strstr(filepath, ".png")) {
-		printf("dtex_load_image 1\n");
-
 		int w, h, c, f;
 		uint8_t* p = dtex_png_read(filepath, &w, &h, &c, &f);
-
-		printf("dtex_load_image 1.5 %d %d\n", w, h);
-
+		_pre_muilti_alpha(p, w, h);
 		int id = dtex_gl_create_texture(DTEX_TF_RGBA8, w, h, p, c, 0);
-
-		printf("dtex_load_image 1.6 %d\n", id);
-
 		free(p);
-
-		printf("dtex_load_image 2\n");
 
 		tex = dtex_texture_create_raw(0);
 		tex->t.RAW.format = DTEX_PNG8;
@@ -474,8 +477,6 @@ dtex_load_image(const char* filepath) {
 		tex->inv_width = 1.0f / w;
 		tex->inv_height= 1.0f / h;
 		tex->id = id;
-		printf("dtex_load_image 3\n");
-
 	}
 	return tex;
 }
